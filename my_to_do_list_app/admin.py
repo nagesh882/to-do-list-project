52dd34d30a527c2a2514e60cd9cf0e2334592273
Nagesh <yashchavan192001@gmail.com>
2024-04-06 02:08:36 +0530

    I have created models.py for database table and admins.py for visualize table in django admin panel

--- my_to_do_list_app/admin.py
+++ my_to_do_list_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from my_to_do_list_app.models import ToDoList
 
-# Register your models here.
+
+@admin.register(ToDoList)
+class ToDoListAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'descripton', 'created_at')
+
+    list_display_links = ('title',)
+
+    readonly_fields = ('created_at',)
+
+    ordering = ('-id',)
--- my_to_do_list_app/models.py
+++ my_to_do_list_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ToDoList(models.Model):
+
+    title = models.CharField(max_length=255, default=None, null=False)
+    descripton = models.TextField(default=None, null=False)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
